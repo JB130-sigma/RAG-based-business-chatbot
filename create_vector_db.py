56d036416d5b7a9ea65f55203e94e091c880cdd4
JB130-sigma <jeetcbhandari@gmail.com>
2026-06-27 22:30:37 +0530

    Update create_vector_db.py

--- create_vector_db.py
+++ create_vector_db.py
@@ -1,29 +1,36 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+import shutil
 
+# Load knowledge base
 loader = TextLoader("data/company_faqs.txt")
-
 documents = loader.load()
 
+# Split into chunks
 splitter = RecursiveCharacterTextSplitter(
-    chunk_size=120,
-    chunk_overlap=20
+    chunk_size=500,
+    chunk_overlap=50
 )
 
-chunks = splitter.split_documents(documents)
+docs = splitter.split_documents(documents)
 
+# Embedding model
 embedding = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
-vectordb = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding,
+# Delete old vector database
+if os.path.exists("vectorstore"):
+    shutil.rmtree("vectorstore")
+
+# Create new vector database
+db = Chroma.from_documents(
+    docs,
+    embedding,
     persist_directory="vectorstore"
 )
 
-vectordb.persist()
-
-print("Vector Database Created Successfully")
+print("✅ Vector Database Created Successfully")
